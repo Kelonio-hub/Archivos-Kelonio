e14c52e1e660a2640b99d9b136b5575d8d93ddba
Kelonio 3DS <67707384+Kelonio-hub@users.noreply.github.com>
2026-09-19 23:11:10 +0200

    Fix indentation for KELONIO_YOUTUBE_CONFIG

--- js/youtube-auth-config.js
+++ js/youtube-auth-config.js
@@ -1,6 +1,6 @@
-  window.KELONIO_YOUTUBE_CONFIG = Object.freeze({
-    clientId: '316282527970-k8roorg133u98gcra57n8qe72bkkkbkk.apps.googleusercontent.com',
-    channelId: 'UCJbYmHLNcrPUUA9oyBGtsKw',
-    channelUrl: 'https://www.youtube.com/channel/UCJbYmHLNcrPUUA9oyBGtsKw',
-    joinUrl: 'https://www.youtube.com/channel/UCJbYmHLNcrPUUA9oyBGtsKw/join'
-  });
+window.KELONIO_YOUTUBE_CONFIG = Object.freeze({
+  clientId: '316282527970-k8roorg133u98gcra57n8qe72bkkkbkk.apps.googleusercontent.com',
+  channelId: 'UCJbYmHLNcrPUUA9oyBGtsKw',
+  channelUrl: 'https://www.youtube.com/channel/UCJbYmHLNcrPUUA9oyBGtsKw',
+  joinUrl: 'https://www.youtube.com/channel/UCJbYmHLNcrPUUA9oyBGtsKw/join'
+});
